959a6f4c1260f8758996fea31641739b3894e9fc
Lyuboslav Ilkov <LyuboslavIlkov@gmail.com>
2020-01-09 19:35:46 +0200

    Updates

---
+++ precache-manifest.7b0aec556500194227ffd37c52ce87ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b35ca5cd598036c933ac44dc92b8416",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2e8e5b5305b1ee62eb6b",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "041c0833e637ab4b2b30",
+    "url": "/portfolio/static/js/2.9626bf7d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.9626bf7d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e8e5b5305b1ee62eb6b",
+    "url": "/portfolio/static/js/main.5a3432cf.chunk.js"
+  },
+  {
+    "revision": "05d36a258d9ef72d28aa",
+    "url": "/portfolio/static/js/runtime-main.14105eb5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
